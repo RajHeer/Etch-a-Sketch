c19e51b7848f46cbe37d898f7430bed8e36335b6
Raj Heer <thisisrajmatazz@gmail.com>
2022-10-14 07:38:14 +0100

    Move grid into section so main can be styled separately

--- main.js
+++ main.js
@@ -2,15 +2,17 @@
 const container = document.querySelector('.container');
 
 function makeGrid (size) {
-    container.style.cssText = `display: grid; grid-template-columns: repeat(${size}, 1fr);`
+    const section = document.createElement('section');
+    section.style.cssText = `display: grid; grid-template-columns: repeat(${size}, 1fr);`
 
     // for loop written avoid zero index when numbering sqs // 
     for (let i = 1; i < (size*size)+1; i++) {
         const square = document.createElement('div');
         square.style.cssText = 'border: solid black 1px; height: 50px';
         square.textContent = i;
-        container.appendChild(square);
+        section.appendChild(square);
     }
+    container.appendChild(section);
 };
 
 const size = prompt('How many rows and columns for your grid?');
